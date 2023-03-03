76e5f77ea172c61b3163352e757a087122ff0df8
Charles <charllesrocklp@gmail.com>
2023-03-03 12:47:49 -0300

    criado modulo carro para executar os testes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,14 @@
 class Pessoa:
-    def comprimentar(self):
+    def __init__(self, nome='Charles Silva'): # o __init__ é um método
+        self.nome = nome # o ATRIBUTO e o nome que esta conectado com o self no caso o sel.nome
+        # os atributos de instancia e de objetos são criados atraves do metodo especial __init__
+
+    def comprimentar(self): # o comprimentar é um método
         return 'Olá'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Noam') # podemos alterar o nome do objeto já na construção
     print(p.comprimentar())
+    print(p.nome)
+    p.nome = 'Silva'
